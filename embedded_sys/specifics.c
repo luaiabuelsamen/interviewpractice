30d472d29a73482e4af97efd45968c2baab29d8c
labuelsamen <luai.abuelsamen@mail.mcgill.ca>
2024-12-05 01:12:18 -0800

    oof

---
+++ embedded_sys/specifics.c
@@ -0,0 +1,31 @@
+/*
+32 bit single core controler
+some sram at 0x1000
+I want to use 128 bit of sram as a bitmap
+write a function to test ad flip
+if bit is set, clear
+if bit is clear set it
+in both cases return boolean old value
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BIT_SIZE 32
+
+uint8_t test_and_flip (volatile uint32_t* pmemstart, uint8_t bit){
+    int mapIndex = bit / BIT_SIZE;
+    int bitIndex = bit % BIT_SIZE;
+    volatile uint32_t* word = &pmemstart[mapIndex];
+    uint8_t oldValue = (*word & (1 << bitIndex)) != 0;
+    *word ^= 1 << bitIndex;
+    return oldValue;
+}
+
+int main(){
+    volatile uint32_t bitmap[4] = {0};
+
+    uint8_t old_value = test_and_flip(bitmap, 5);
+    printf("Old value: %d, New word value: 0x%X\n", old_value, bitmap[0]);
+
+    return 0;
+}
